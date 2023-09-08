6f1413f6976da12470fa2167f5f03abc8bc594ce
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-09-08 02:12:25 +0200

    Initial commit

--- main.py
+++ main.py
@@ -6,39 +6,43 @@ import cv2
 
 
 def watermarkAdder():
-    file1 = browseFile()
-    file2 = browseFile()
-    logo = Image.open(file2).convert("RGBA")
-    product = cv2.imread(file1)
+    image_file = browseFile("Select an image",
+                            [("Image files", ["*.jpg", "*.png", "*.webp"]), ("All files", "*.*")])
+    logo_file = browseFile("Select a logo", [("Image files", ["*.png"]), ("All files", "*.*")])
+
+    if not image_file or not logo_file:
+        return
+
+    watermark = Image.open(logo_file).convert("RGBA")
+    product = cv2.imread(image_file)
 
     product_size = product.shape[:2]
-    logo = logo.resize((int(product_size[1] * 0.2), int(product_size[0] * 0.2)))
+    watermark = watermark.resize((int(product_size[1] * 0.2), int(product_size[0] * 0.2)))
 
-    logo_width, logo_height = logo.size
-    position = (product_size[1] - logo_width, product_size[0] - logo_height)
+    watermark_width, watermark_height = watermark.size
+    position = (product_size[1] - watermark_width, product_size[0] - watermark_height)
 
-    logo_cv = np.array(logo)
-    alpha_channel = logo_cv[:, :, 3]
+    watermark_cv = np.array(watermark)
+    alpha_channel = watermark_cv[:, :, 3]
 
-    # Create a mask for the alpha channel
     alpha_mask = alpha_channel / 255.0
     product_copy = product.copy()
+
     for c in range(0, 3):
-        product[position[1]:position[1] + logo_height, position[0]:position[0] + logo_width, c] = \
-            (1 - alpha_mask) * product[position[1]:position[1] + logo_height,position[0]:position[0] + logo_width, c] + \
-            alpha_mask * logo_cv[:, :, c]
+        product[position[1]:position[1] + watermark_height, position[0]:position[0] + watermark_width, c] = \
+            (1 - alpha_mask) * product[position[1]:position[1] + watermark_height,
+                               position[0]:position[0] + watermark_width, c] + \
+            alpha_mask * watermark_cv[:, :, c]
 
-    cv2.imwrite('watermarked_image.jpg', product)
-    print("Watermark successfully added!")
+    output_file = 'watermarked_image.jpg'
+    cv2.imwrite(output_file, product)
+    print(f"Watermark successfully added to {output_file}")
 
 
-def browseFile():
+def browseFile(title,filetype):
     filename = filedialog.askopenfilename(initialdir="/",
-                                          title="Select a File",
-                                          filetypes=(("Image files",
-                                                      ["*.jpg*", "*.png*", "*.webp*"]),
-                                                     ("all files",
-                                                      "*.*")))
+                                          title=title,
+                                          filetypes=filetype)
     return filename
 
 
